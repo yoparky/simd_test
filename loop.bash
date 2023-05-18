3e8052880129d8b0c68f058a5504f5ca221edf85
Young-Joon <yopark@sas.upenn.edu>
2023-05-18 12:43:09 +0900

    change script

--- loop.bash
+++ loop.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-for n in 1024 4096 16384 65536 10000 262144 1048576 4194304
+for n in 1024 4096 16384 65536 262144 1048576 4194304
 do
   g++  proj04.cpp  -DARRAYSIZE=$n  -o proj04  -lm  -fopenmp
   ./proj04
